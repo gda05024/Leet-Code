b872f5540bb66393535ce15e02402fc4dff0502c
Yeeun kim <gda05024@naver.com>
2025-07-16 23:09:04 +0900

    Time: 40 ms (67.6%), Space: 52.4 MB (95.31%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,13 @@
+
+let expect = function (val) {
+    return {
+        toBe: function(other) {
+            if(val === other) return true;
+            throw new Error("Not Equal");
+        },
+        notToBe: function(other) {
+            if(val !== other) return true;
+            throw new Error("Equal");
+        }
+    }
+}
